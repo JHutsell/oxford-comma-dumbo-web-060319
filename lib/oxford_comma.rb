3a3cbfae9ae1b287135cee17f5c9dbcc0209784c
Justin Hutsell <hutselljustin@gmail.com>
2019-05-30 04:27:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,7 @@ def oxford_comma(array)
   oxford_comma_sent = ""
   array.each_with_index do |str, idx|
     oxford_comma_sent << "#{str}, " unless str == array.last
-    oxford_comma_sent << "and #{str}."
+    oxford_comma_sent << "and #{str}"
   end
+  oxford_comma_sent
 end
