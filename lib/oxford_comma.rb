ad52aab270a140d1ea3aa0d3557791ac2d407c48
Justin Hutsell <hutselljustin@gmail.com>
2019-05-30 04:30:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,11 @@
 def oxford_comma(array)
   oxford_comma_sent = ""
-  array.each_with_index do |str, idx|
-    oxford_comma_sent << "#{str}, " unless str == array.last
-    oxford_comma_sent << "and #{str}"
-  end
+  # array.each_with_index do |str, idx|
+  #   oxford_comma_sent << "#{str}, " unless str == array.last
+  #   oxford_comma_sent << "and #{str}"
+  # end
+  # oxford_comma_sent
+  array[0...-1].each { |str| oxford_comma_sent << "#{str}, " }
+  oxford_comma_sent << "and #{array.last}"
   oxford_comma_sent
 end
