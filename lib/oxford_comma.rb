45f0a6c75aeaf8eed481d48e2972f5715c6fdeed
Justin Hutsell <hutselljustin@gmail.com>
2019-05-30 04:26:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
 def oxford_comma(array)
-
+  oxford_comma_sent = ""
+  array.each_with_index do |str, idx|
+    oxford_comma_sent << "#{str}, " unless str == array.last
+    oxford_comma_sent << "and #{str}."
+  end
 end
